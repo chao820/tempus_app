e8d6917125329a3fb3771b6d42716f89cba457f1
chao820 <chaozr20@gmail.com>
2026-05-30 01:42:24 +0800

    Update vite.config.ts

--- tempus-逆時防線/vite.config.ts
+++ tempus-逆時防線/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc' // 或者 @vitejs/plugin-react，看你原本是哪一個
+import react from '@vitejs/react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tempus-逆時防線/', // 🔥 必須加上這行，告訴瀏覽器你的資料夾叫這個名字！
+  base: './', // 🔥 直接改成 './'，用相對路徑徹底解決所有目錄迷路問題！
   plugins: [react()],
 })
